e6b351b778997edd501212bd2bb4f031c58124ab
fsantanna <francisco.santanna@gmail.com>
2021-02-20 07:03:06 -0300

    (>) PushbackReader

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,12 +1,19 @@
 import org.junit.jupiter.api.MethodOrderer.Alphanumeric
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
+import java.io.PushbackReader
+import java.io.StringReader
 
 @TestMethodOrder(Alphanumeric::class)
 class Tests {
 
     @Test
-    fun a00_hello () {
-        println("Hello World!")
+    fun a00_buffer () {
+        val inp = PushbackReader(StringReader("Hello World!"))
+        assert(inp.read().toChar() == 'H')
+        val c = inp.read()
+        assert(c.toChar() == 'e')
+        inp.unread(c)
+        assert(inp.read().toChar() == 'e')
     }
 }
