f984e95f8976f2d2fbc678af0e4b25fa4aeed36c
Francisco Sant'Anna <francisco.santanna@gmail.com>
2021-09-21 22:01:36 -0300

    ...

--- src/main/kotlin/Simul.kt
+++ src/main/kotlin/Simul.kt
@@ -100,7 +100,7 @@ fun Stmt.simul (
         is Stmt.Set   -> this.src.simul(st, fs, fx, fe, listOf(this.dst)+nxts)
         is Stmt.Call  -> this.call.simul(st, fs, fx, fe, nxts)
         is Stmt.Break -> nxt(st, fs, fx, fe, stack_rem { it is Stmt.Loop })
-        is Stmt.Ret   -> nxt(st, fs, fx, fe, stack_rem { it is Expr.Call })
+        is Stmt.Ret   -> {} //nxt(st, fs, fx, fe, stack_rem { it is Expr.Call })
         is Stmt.Block -> this.body.simul(st, fs, fx, fe, nxts)
         is Stmt.Seq   -> this.s1.simul(st, fs, fx, fe, listOf(this.s2)+nxts)
         is Stmt.Loop  -> {
