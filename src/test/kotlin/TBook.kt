661148f34bca046f8a03b2fac257670fb1631c67
Francisco Sant'Anna <francisco.santanna@gmail.com>
2021-12-18 11:35:06 -0300

    ...

--- src/test/kotlin/TBook.kt
+++ src/test/kotlin/TBook.kt
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.MethodOrderer.Alphanumeric
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
@@ -10,6 +11,8 @@ val nums = """
     set one = new <.1 zero>:</^ @local> @local
     var two: /</^ @local> @local
     set two = new <.1 one>:</^ @local> @local
+    var three: /</^ @local> @local
+    set three = new <.1 two>:</^ @local> @local
 """.trimIndent()
 
 fun Num (ptr: Boolean, scope: String): String {
@@ -70,33 +73,34 @@ val lt = """
 @TestMethodOrder(Alphanumeric::class)
 class TBook {
 
-    fun all (inp: String): String {
-        println("nums: ${nums.count { it=='\n' }}")
-        println("add:  ${add.count  { it=='\n' }}")
-        println("mul:  ${mul.count  { it=='\n' }}")
-        println("lt:   ${lt.count   { it=='\n' }}")
+    fun all(inp: String): String {
+        println("nums: ${nums.count { it == '\n' }}")
+        println("add:  ${add.count { it == '\n' }}")
+        println("mul:  ${mul.count { it == '\n' }}")
+        println("lt:   ${lt.count { it == '\n' }}")
 
-        val (ok1,out1) = All_inp2c(inp)
+        val (ok1, out1) = All_inp2c(inp)
         if (!ok1) {
             return out1
         }
         File("out.c").writeText(out1)
-        val (ok2,out2) = exec("gcc -Werror out.c -o out.exe")
+        val (ok2, out2) = exec("gcc -Werror out.c -o out.exe")
         if (!ok2) {
             return out2
         }
-        val (_,out3) = exec("./out.exe")
+        val (_, out3) = exec("./out.exe")
         //val (_,out3) = exec("valgrind ./out.exe")
-            // search in tests output for
-            //  - "definitely lost"
-            //  - "Invalid read of size"
+        // search in tests output for
+        //  - "definitely lost"
+        //  - "Invalid read of size"
         //println(out3)
         return out3
     }
 
     @Test
-    fun pre_01_nums () {
-        val out = all("""
+    fun pre_01_nums() {
+        val out = all(
+            """
             var zero: /</^ @local> @local
             set zero = <.0>: /</^ @local> @local
             var one: </^ @local>
@@ -104,44 +108,55 @@ class TBook {
             var two: </^ @local>
             set two = <.1 /one>: </^ @local>
             output std /two
-        """.trimIndent())
+        """.trimIndent()
+        )
         assert(out == "<.1 <.1 <.0>>>\n") { out }
     }
+
     @Test
-    fun pre_03_add () {
-        val out = all("""
+    fun pre_03_add() {
+        val out = all(
+            """
             $nums
             $add
             output std add [two,one]
-        """.trimIndent())
+        """.trimIndent()
+        )
         assert(out == "<.1 <.1 <.1 <.0>>>>\n") { out }
     }
+
     @Test
-    fun pre_04_mul () {
-        val out = all("""
+    fun pre_04_mul() {
+        val out = all(
+            """
             $nums
             $add
             $mul
             output std mul [two, add [two,one]]
-        """.trimIndent())
+        """.trimIndent()
+        )
         assert(out == "<.1 <.1 <.1 <.1 <.1 <.1 <.0>>>>>>>\n") { out }
     }
+
     @Test
-    fun pre_05_lt () {
-        val out = all("""
+    fun pre_05_lt() {
+        val out = all(
+            """
             $nums
             $lt
             output std lt [two, one]
             output std lt [one, two]
-        """.trimIndent())
+        """.trimIndent()
+        )
         assert(out == "0\n1\n") { out }
     }
 
     // CHAPTER 1.1
 
     @Test
-    fun ch_01_01_square_pg02 () {
-        val out = all("""
+    fun ch_01_01_square_pg02() {
+        val out = all(
+            """
             $nums
             $add
             $mul
@@ -150,12 +165,15 @@ class TBook {
                 return mul [arg,arg] @1
             }
             output std square two
-        """.trimIndent())
+        """.trimIndent()
+        )
         assert(out == "<.1 <.1 <.1 <.1 <.0>>>>>\n") { out }
     }
+
     @Test
-    fun ch_01_01_smaller_pg02 () {
-        val out = all("""
+    fun ch_01_01_smaller_pg02() {
+        val out = all(
+            """
             $nums
             $lt
             var smaller: [$NumT1,$NumT1] -> $NumT1
@@ -168,7 +186,114 @@ class TBook {
             }
             output std smaller [one,two] @local
             output std smaller [two,one] @local
-        """.trimIndent())
+        """.trimIndent()
+        )
         assert(out == "<.1 <.0>>\n<.1 <.0>>\n") { out }
+    }
+
+    @Test
+    fun ch_01_01_delta_pg03() {
+        println("TODO")
+    }
+
+    // CHAPTER 1.2
+
+    @Test
+    fun ch_01_02_three_pg05() {
+        val out = all(
+            """
+            $nums
+            var f_three: $NumT1 -> $NumT1
+            set f_three = func $NumT1 -> $NumT1 {
+                return three
+            }
+            output std f_three one
+        """.trimIndent()
+        )
+        assert(out == "<.1 <.1 <.1 <.0>>>>\n") { out }
+    }
+    @Disabled // TODO: infinite loop
+    @Test
+    fun ch_01_02_infinity_pg05() {
+        val out = all(
+            """
+            var infinity: () -> $NumT1
+            set infinity = func () -> $NumT1 {
+                output std _10:_int
+                return new <.1 infinity() @1>:$NumF1 @1
+            }
+            output std infinity ()
+        """.trimIndent()
+        )
+        assert(out == "<.1 <.1 <.1 <.0>>>>\n") { out }
+    }
+
+    // CHAPTER 1.3
+
+    @Test
+    fun ch_01_03_multiply_pg09() {
+        val out = all(
+            """
+            $nums
+            $add
+            $mul
+            var multiply: [$NumT1,$NumT1] -> $NumT1
+            set multiply = func [$NumT1,$NumT1] -> $NumT1 {
+                if arg.1\?0 {
+                    return <.0>:${NumT1}
+                } else {
+                    return mul arg @1
+                }
+            }
+            output std multiply [two,three]
+        """.trimIndent()
+        )
+        assert(out == "<.1 <.1 <.1 <.1 <.1 <.1 <.0>>>>>>>\n") { out }
+    }
+
+    // CHAPTER 1.4
+
+    @Disabled  // TODO: currying requires returning function
+    @Test
+    fun ch_01_04_currying_pg11() {
+        val out = all(
+            """
+            $nums
+            $lt
+            var smallerc: $NumT1 -> ($NumT1->$NumT1)
+            set smallerc = func $NumT1 -> ($NumT1->$NumT1) {
+                var x: $NumT1
+                set x = arg
+                return func $NumT1 -> $NumT1 {  -- would require annotation to hold x (func [x] ...)
+                    return lt [x,arg]
+                }
+            }
+            var f: $NumTL -> $NumTL
+            set f = smallerc two   -- smallerc could keep two in memory as long as smallerc does not live longer than two
+            output std f one
+            output std f three
+        """.trimIndent()
+        )
+        assert(out == "<.1 <.0>>\n<.1 <.1 <.0>>>\n") { out }
+    }
+    @Test
+    fun ch_01_04_twice_pg11() {
+        val out = all(
+            """
+            $nums
+            $add
+            $mul
+            var square: $NumT1 -> $NumT1
+            set square = func $NumT1 -> $NumT1 {
+                return mul [arg,arg] @1
+            }
+            var twice: [$NumT1->$NumT1, $NumT1] -> $NumT1
+            set twice = func [$NumT1->$NumT1, $NumT1] -> $NumT1 {
+                return arg.1 arg.1 arg.2 @1 @1
+            }
+            output std twice [square,two] @local
+        """.trimIndent()
+        )
+        assert(out == "<.1 <.0>>\n<.1 <.1 <.0>>>\n") { out }
     }
 }
