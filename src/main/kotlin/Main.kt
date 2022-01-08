29b11f5ff396bed7084ef82018d382db8139431a
Francisco Sant'Anna <francisco.santanna@gmail.com>
2022-01-08 09:45:04 -0300

    - refactor Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,18 @@ import java.io.File
 import java.io.PushbackReader
 import java.io.StringReader
 
-fun All_inp2c (inp: String): Pair<Boolean,String> {
-    val all = All_new(PushbackReader(StringReader(inp), 2))
+fun exec (cmd: String): Pair<Boolean,String> {
+    val p = ProcessBuilder(cmd.split(' '))
+        //.redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectErrorStream(true)
+        .start()
+    val ret = p.waitFor()
+    val str = p.inputStream.bufferedReader().readText()
+    return Pair(ret==0, str)
+}
+
+fun ce2c (ce: String): Pair<Boolean,String> {
+    val all = All_new(PushbackReader(StringReader(ce), 2))
     lexer(all)
     try {
         var s = parser_stmts(all, Pair(TK.EOF,null))
@@ -20,29 +30,29 @@ fun All_inp2c (inp: String): Pair<Boolean,String> {
     }
 }
 
-fun exec (cmd: String): Pair<Boolean,String> {
-    val p = ProcessBuilder(cmd.split(' '))
-        //.redirectOutput(ProcessBuilder.Redirect.PIPE)
-        .redirectErrorStream(true)
-        .start()
-    val ret = p.waitFor()
-    val str = p.inputStream.bufferedReader().readText()
-    return Pair(ret==0, str)
+fun c2exe (c: String): Pair<Boolean,String> {
+    File("out.c").writeText(c)
+    return exec("gcc out.c -o out.exe")
+}
+
+fun exe2run (): Pair<Boolean,String> {
+    return exec("./out.exe")
+    //return exec("valgrind ./out.exe")
 }
 
 fun main (args: Array<String>) {
     val inp = File(args[0]).readText()
-    val (ok1,out1) = All_inp2c(inp)
+    val (ok1,out1) = ce2c(inp)
     if (!ok1) {
         println(out1)
+        return
     }
-    File("out.c").writeText(out1)
-    val (ok2,out2) = exec("gcc out.c -o out.exe")
+    val (ok2,out2) = c2exe(out1)
     if (!ok2) {
         println(out2)
+        return
     }
-    val (_,out3) = exec("./out.exe")
-    //val (_,out3) = exec("valgrind ./out.exe")
+    val (_,out3) = exe2run()
     print(out3)
 }
 
