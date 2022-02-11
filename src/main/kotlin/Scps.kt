cacfaaa70518231964e0d3596d47a9eba200d983
Francisco Sant'Anna <francisco.santanna@gmail.com>
2022-02-11 12:57:03 -0300

    - separate Scope.toScp2

--- src/main/kotlin/Scps.kt
+++ src/main/kotlin/Scps.kt
@@ -1,6 +1,14 @@
 // Triple<lvl,par,depth>
 data class Scope (var scp1: Tk.Id, var scp2: Triple<Int,String?,Int?>?)
 
+fun Tk.Id?.isanon (): Boolean {
+    return this.let { (it==null || it.id[0]=='B'&&it.id[1].isDigit()) }
+}
+
+fun Tk.Id.anon2local (): String {
+    return if (this.isanon()) "LOCAL" else this.id
+}
+
 fun Any.localBlock (): String {
     return (this.ups_first { it is Stmt.Block } as Stmt.Block?).let {
         if (it == null) "GLOBAL" else it.scp1!!.id
@@ -16,23 +24,27 @@ fun Stmt.setScp1s () {
     this.visit(null, null, null, ::fx)
 }
 
-fun Stmt.setScp2s () {
-    fun fx (up: Any, scp: Scope) {
-        val lvl = up.ups_tolist().filter { it is Expr.Func }.count() // level of function nesting
-        scp.scp2 = when (scp.scp1.id) { // 2xExpr.Func, otherwise no level between outer/arg/body
-            "GLOBAL" -> Triple(0, null, 0)
-            else -> {
-                val blk = up.env(scp.scp1.id)
-                if (blk != null) {
-                    // @A, @x, ...
-                    val one = if (blk is Stmt.Block) 1 else 0
-                    val umn = if (scp.scp1.id=="arg")    1 else 0   // "arg" is in between Func-arg-Block
-                    Triple(lvl, null, one - umn + blk.ups_tolist().let { it.count{it is Stmt.Block} + 2*it.count{it is Expr.Func} })
-                } else {
-                    Triple(lvl, scp.scp1.id, null)
-                }
+fun Scope.toScp2 (up: Any): Triple<Int,String?,Int?> {
+    val lvl = up.ups_tolist().filter { it is Expr.Func }.count() // level of function nesting
+    return when (this.scp1.id) { // 2xExpr.Func, otherwise no level between outer/arg/body
+        "GLOBAL" -> Triple(0, null, 0)
+        else -> {
+            val blk = up.env(this.scp1.id)
+            if (blk != null) {
+                // @A, @x, ...
+                val one = if (blk is Stmt.Block) 1 else 0
+                val umn = if (this.scp1.id=="arg")    1 else 0   // "arg" is in between Func-arg-Block
+                Triple(lvl, null, one - umn + blk.ups_tolist().let { it.count{it is Stmt.Block} + 2*it.count{it is Expr.Func} })
+            } else {
+                Triple(lvl, this.scp1.id, null)
             }
         }
+    }
+}
+
+fun Stmt.setScp2s () {
+    fun fx (up: Any, scp: Scope) {
+        scp.scp2 = scp.toScp2(up)
     }
     this.visit(null, null, null, ::fx)
 }
